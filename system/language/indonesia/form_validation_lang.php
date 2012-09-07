8cfeb66b444a493df11b6b326021d93310c1e91f
Navotera <navotera@yahoo.co.id>
2012-09-07 11:43:00 +0700

    uol

---
+++ system/language/indonesia/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['required']			= " %s harus diisi.";
+$lang['isset']				= " %s field must have a value.";
+$lang['valid_email']		= " %s diisi dengan email address valid.";
+$lang['valid_emails']		= " %s field must contain all valid email addresses.";
+$lang['valid_url']			= " %s field must contain a valid URL.";
+$lang['valid_ip']			= " %s field must contain a valid IP.";
+$lang['min_length']			= " %s field must be at least %s characters in length.";
+$lang['max_length']			= " %s kolom tidak boleh melebihi %s karakter.";
+$lang['exact_length']		= " %s field must be exactly %s characters in length.";
+$lang['alpha']				= " %s field may only contain alphabetical characters.";
+$lang['alpha_numeric']		= " %s field may only contain alpha-numeric characters.";
+$lang['alpha_dash']			= " %s field may only contain alpha-numeric characters, underscores, and dashes.";
+$lang['numeric']			= " %s field must contain only numbers.";
+$lang['is_numeric']			= " %s field must contain only numeric characters.";
+$lang['integer']			= " %s field must contain an integer.";
+$lang['regex_match']		= " %s field is not in  correct format.";
+$lang['matches']			= " %s field does not match  %s field.";
+$lang['is_unique'] 			= " %s kolom harus unik.";
+$lang['is_natural']			= " %s diisi dengan angka positif.";
+$lang['is_natural_no_zero']	= " %s field must contain a number greater than zero.";
+$lang['decimal']			= " %s diisi dengan angka desimal.";
+$lang['less_than']			= " %s diisi angka yang kurang dari %s.";
+$lang['greater_than']		= " %s diisi angka yang lebih besar dari %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
